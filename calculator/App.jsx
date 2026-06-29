9959de85b74fc48bebe7b5d886d838aaa1667a79
y-omura-y <y.omura@digi.jp>
2026-06-29 16:23:29 +0900

    四則演算子の反映

--- calculator/App.jsx
+++ calculator/App.jsx
@@ -2,6 +2,14 @@ const {useState} = React;
 
 function App(){
 const [display,setDisplay] = useState("0");
+const [memory,setMemory] = useState(null);
+const [operator,setOperator] = useState(null);
+
+function handleOperator(op){
+  setMemory(display);
+  setOperator(op);
+  setDisplay("0");
+}
 
   function handleKey(key){
     if(display === "0"){
@@ -10,6 +18,21 @@ const [display,setDisplay] = useState("0");
       setDisplay(display + key);
     }
   }
+
+  function handleEqual() {
+  const left = parseFloat(memory);
+  const right = parseFloat(display);
+
+  let result;
+  if (operator === "+") result = left + right;
+  if (operator === "-") result = left - right;
+  if (operator === "×") result = left * right;
+  if (operator === "÷") result = left / right;
+
+  setDisplay(String(result));
+  setMemory(null);
+  setOperator(null);
+}
 
   return(
     <div className="calc">
@@ -20,27 +43,27 @@ const [display,setDisplay] = useState("0");
   <button className="btn" onClick={() => handleKey("7")}>7</button>
   <button className="btn" onClick={() => handleKey("8")}>8</button>
   <button className="btn" onClick={() => handleKey("9")}>9</button>
-  <button className="btn" onClick={() => handleKey("")}>×</button>
+  <button className="btn" onClick={() => handleOperator("×")}>×</button>
 </div>
 
 <div className="row">
   <button className="btn" onClick={() => handleKey("4")}>4</button>
   <button className="btn" onClick={() => handleKey("5")}>5</button>
   <button className="btn" onClick={() => handleKey("6")}>6</button>
-  <button className="btn" onClick={() => handleKey("")}>-</button>
+  <button className="btn" onClick={() => handleOperator("-")}>-</button>
 </div>
 
 <div className="row">
   <button className="btn" onClick={() => handleKey("1")}>1</button>
   <button className="btn" onClick={() => handleKey("2")}>2</button>
   <button className="btn" onClick={() => handleKey("3")}>3</button>
-  <button className="btn" onClick={() => handleKey("")}>+</button>
+  <button className="btn" onClick={() => handleOperator("+")}>+</button>
 </div>
 <div className="row">
   <button className="btn" onClick={() => handleKey("")}>c</button>
   <button className="btn" onClick={() => handleKey("0")}>0</button>
-  <button className="btn" onClick={() => handleKey("")}>=</button>
-  <button className="btn" onClick={() => handleKey("")}>÷</button>
+  <button className="btn" onClick={() => handleEqual()}>=</button>
+  <button className="btn" onClick={() => handleOperator("÷")}>÷</button>
 </div>
     </div>
   )
