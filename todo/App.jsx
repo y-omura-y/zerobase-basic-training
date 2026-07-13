dac4a510dd8d5c01dbab2180bc2d96c164733fb6
y-omura-y <y.omura@digi.jp>
2026-07-13 11:16:17 +0900

    画面描写

--- todo/App.jsx
+++ todo/App.jsx
@@ -1,5 +1,32 @@
 const { useState } = React;
 
+function App(){
+
+  
+
+return(
+  <div>
+    <h1>Todo List</h1>
+
+    <div className="input-row">
+      <input type="text" placeholder="タスク入力"/>
+      <button>追加</button>
+    </div>
+
+      <div className="task-box">
+<ul>
+  <li>
+    <input type="checkbox" />
+    <h2>タスク</h2>
+    <button>けす</button>
+  </li>
+</ul>
+      </div>
+    
+  </div>
+)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
